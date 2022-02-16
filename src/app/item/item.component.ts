a1c044427f5e0535a76181ddb746df48fdf3d44b
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-02-16 13:00:58 -0800

    2 files modified, 1 added

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
+  @Input() text!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
